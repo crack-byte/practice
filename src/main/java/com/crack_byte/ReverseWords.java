d6d62089dbe0054885b5e87fb19dc0e05e01dc49
abhishek <abhishek.purohit@oodles.io>
2021-10-15 01:02:19 +0530

    1. reverse words

---
+++ src/main/java/com/crack_byte/ReverseWords.java
@@ -0,0 +1,36 @@
+package com.crack_byte;
+
+public class ReverseWords {
+    public static String reverseWords(String s) {
+        return iterate(s.toCharArray());
+    }
+    public static String iterate(char[] chars) {
+        int len = chars.length;
+        int low = 0;
+        int high = 0;
+        while (high < len) {
+            if (chars[high] == ' ' || high==len-1) {
+                if(high==len-1){
+                    reverseString2(chars, low, high );
+                    break;
+                }else {
+                    reverseString2(chars, low, high - 1);
+                }
+                low = high + 1;
+            }
+            high++;
+        }
+        return String.valueOf(chars);
+    }
+
+
+    public static void reverseString2(char[] s, int low, int high) {
+        while (high > low) {
+            char c = s[high];
+            s[high] = s[low];
+            s[low] = c;
+            high--;
+            low++;
+        }
+    }
+}
