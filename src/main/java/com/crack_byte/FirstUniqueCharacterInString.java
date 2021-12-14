ab7c6252bb8e588a6873fd99eb1f190f46c08bee
abhishek <abhishek.purohit>
2021-12-14 19:31:43 +0530

    updates

---
+++ src/main/java/com/crack_byte/FirstUniqueCharacterInString.java
@@ -0,0 +1,22 @@
+package com.crack_byte;
+
+public class FirstUniqueCharacterInString {
+    public static int firstUniqChar(String s) {
+        int index = 100000, max = index;
+        int c = 97;
+        while (c <= 122) {
+            int i = s.indexOf(c);
+            if (i >= 0 && i == s.lastIndexOf(c)) {
+                if (i < index) {
+                    index = i;
+                }
+            }
+            ++c;
+        }
+        return index == max ? -1 : index;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstUniqChar("loveleetcode"));
+    }
+}
