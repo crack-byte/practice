7fc133e193fee5e69c047f3efd34773760e9e7fb
Abhishek Purohit <abhi.purohit1997@protonmail.com>
2022-02-07 20:20:52 +0530

    1. added a simple program for explanation

---
+++ src/main/java/com/crack_byte/Test.java
@@ -0,0 +1,14 @@
+package com.crack_byte;
+
+public class Test {
+    public static void main(String[] args) {
+        int a = 1, i = 2, b = 0;
+        do {
+            int i1 = b + a;
+            int i2 = i1 * i;
+            System.out.println(i+" x" + (i1 / 10 < 1 ? ' ' : "") + i1 + ' ' + "=" + (i2 / 10 < 1 ? ' ' : "") + i2);
+            a++;
+        }
+        while (a <= 10);
+    }
+}
