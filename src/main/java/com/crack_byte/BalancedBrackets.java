bcd3992b62bfa13c2cbea78e3115f799fde6da07
abhishekpurohit <abhishek.purohit>
2022-06-29 19:05:46 +0530

    added a new simple program to find balanced brackets

---
+++ src/main/java/com/crack_byte/BalancedBrackets.java
@@ -0,0 +1,56 @@
+package com.crack_byte;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class BalancedBrackets {
+    public static Map<Character,Character> map = new HashMap<>(4);
+    static {
+        map.put('[',']');
+        map.put('(',')');
+        map.put('<','>');
+        map.put('{','}');
+    }
+    public static void main(String[] args) {
+        long l = System.currentTimeMillis();
+        System.out.println(isBalanced("({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])" +
+                "({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])" +
+                "({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])" +
+                "({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])" +
+                "({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])" +
+                "({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])" +
+                "({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])" +
+                "({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])" +
+                "({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])" +
+                "({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])" +
+                "({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])" +
+                "({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])" +
+                "({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])({[])" +
+                "({[])({[])"));
+        System.out.println(System.currentTimeMillis()-l);
+    }
+    public static boolean isBalanced(String str){
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()){
+            if(stack.empty()){
+                stack.push(c);
+            }else {
+                if(isOpen(c)){
+                    stack.push(c);
+                }else {
+                    Character character = map.get(stack.peek());
+                    if (character!=null && character.equals(c)) {
+                        stack.pop();
+                    } else {
+                        stack.push(c);
+                    }
+                }
+            }
+        }
+        return stack.empty();
+    }
+    public static boolean isOpen(char c){
+        return map.containsKey(c);
+    }
+}
